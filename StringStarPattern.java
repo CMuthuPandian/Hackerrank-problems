41e474544cb2efc067c4ee98fc73b7c680bdf248
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-01-25 21:49:29 +0530

    Add files via upload

---
+++ StringStarPattern.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class StringStarPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String s = sc.nextLine();
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = 0; j < s.length(); j++) {
+                if (i == j && i + j != s.length() - 1) {
+                    System.out.print(s.charAt(i));
+                } else if (i + j == s.length() - 1) {
+                    System.out.print(s.charAt(j));
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
