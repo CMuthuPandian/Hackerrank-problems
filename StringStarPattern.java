9dfd008130238273d518240798dfce1bfd3fbd7f
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-01-25 21:53:13 +0530

    Update StringStarPattern.java

--- StringStarPattern.java
+++ StringStarPattern.java
@@ -8,7 +8,8 @@ public class StringStarPattern {
             for (int j = 0; j < s.length(); j++) {
                 if (i == j && i + j != s.length() - 1) {
                     System.out.print(s.charAt(i));
-                } else if (i + j == s.length() - 1) {
+                } 
+                else if (i + j == s.length() - 1) {
                     System.out.print(s.charAt(j));
                 } else {
                     System.out.print(" ");
