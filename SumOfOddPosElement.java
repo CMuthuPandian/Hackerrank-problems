4d4c308989c4eb8daa3a9bb0814a130d867edbd7
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-01-26 22:20:08 +0530

    Add files via upload

---
+++ SumOfOddPosElement.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+//Add the odd positioned elements in an array and print the resultant. start the index position from 0 .
+
+public class SumOfOddPosElement {
+    public static void main(String[] args) {
+        Scanner s  = new Scanner(System.in);
+        int n = s.nextInt();
+        int count =0;
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=s.nextInt();
+        }
+        for(int j=0;j<n;j++){
+            if(j%2==0)
+                count=count;
+            else
+                count=count+arr[j];
+        }
+        System.out.println(count);
+    }
+}
