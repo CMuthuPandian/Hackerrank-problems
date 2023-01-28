df0454dec5db8063b756f850f941fcd723072b43
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-01-28 22:23:40 +0530

    Add files via upload

---
+++ PrimeDigits.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+//Find the prime digits of the number
+
+public class PrimeDigits {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int digit=0,rev=0;
+        while(n!=0){
+            digit=n%10;
+            if(isPrime(digit)==true)
+                rev=(10*rev)+digit;
+            n=n/10;
+        }
+        while(rev!=0){
+            System.out.print((rev%10)+" ");
+            rev=rev/10;
+        }
+    }
+
+    static boolean isPrime(int m){
+        int count=0;
+        for(int i=2;i<=m;i++){
+            if(m%i==0)
+                count++;
+        }
+        if(count==1)
+            return true;
+        else
+            return false;
+    }
+}
