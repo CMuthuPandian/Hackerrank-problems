18a8856c12f2e4ed0757f2b3f50f31e7243262ea
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-01-29 22:00:19 +0530

    Add files via upload

---
+++ PrintWithoutSemicolan.java
@@ -0,0 +1,14 @@
+//Print string without semicolan number ot times specified by user
+
+import java.util.Scanner;
+
+public class PrintWithoutSemicolan {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for(int i=1;i<=n;System.out.println("Hello"))
+            i++;
+    }
+
+}
