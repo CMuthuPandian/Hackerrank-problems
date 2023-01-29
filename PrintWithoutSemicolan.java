c6b521933e197917232c591af241041eefa1324a
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-01-29 22:02:16 +0530

    Update PrintWithoutSemicolan.java

--- PrintWithoutSemicolan.java
+++ PrintWithoutSemicolan.java
@@ -1,7 +1,6 @@
 //Print string without semicolan number ot times specified by user
 import java.util.Scanner;
 public class PrintWithoutSemicolan {
-
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
