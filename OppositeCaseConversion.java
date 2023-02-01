ca66671cddb92da298ced05d311646d417c0f6a3
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-02-01 22:23:43 +0530

    Add files via upload

---
+++ OppositeCaseConversion.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class OppositeCaseConversion {
+    public static void main(String args[] ) throws Exception {
+        Scanner s = new Scanner(System.in);
+        String str = s.nextLine();
+        for(int i=0;i<str.length();i++){
+            if((int)str.charAt(i)>=65 && (int)str.charAt(i)<=90)
+                System.out.print(Character.toLowerCase((str.charAt(i))));
+            else if((int)str.charAt(i)>=97 && (int)str.charAt(i)<=122)
+                System.out.print(Character.toUpperCase((str.charAt(i))));
+            else
+                System.out.print(" ");
+        }
+}
