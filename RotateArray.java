adade19621b7459ff7a62316b6ae823cfdb0bac2
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-02-02 01:06:29 +0530

    Add files via upload

---
+++ RotateArray.java
@@ -0,0 +1,35 @@
+//Rotate the array k number of times while k is user input.
+
+import java.util.Scanner;
+
+public class RotateArray {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=s.nextInt();
+        }
+        int k = s.nextInt();
+        rotateArray(arr,k);
+    }
+
+    public static void rotateArray(int[] nums, int k){
+        int temp;
+        if(nums.length==0)
+            return ;
+        while(k!=0){
+            for(int i=0;i<nums.length-1;i++){
+                temp = nums[i];
+                nums[i]=nums[nums.length-1];
+                nums[nums.length-1]=temp;
+            }
+            k--;
+        }
+        for(int i=0;i<nums.length;i++){
+            System.out.print(nums[i]+" ");
+        }
+    }
+
+}
