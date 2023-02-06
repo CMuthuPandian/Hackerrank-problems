4844dd24395c56842b4016e1a99a6b41d9f7a657
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-02-06 18:48:12 +0530

    Update SunOfElements.java

--- SunOfElements.java
+++ SunOfElements.java
@@ -28,7 +28,6 @@ Explanation 0
  */
 import java.util.Scanner;
 public class SunOfElements {
-
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String str = s.nextLine();
