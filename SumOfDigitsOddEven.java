3aea1bfe50080d9523be5284ba114a4056848bd1
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-02-09 22:07:35 +0530

    Add files via upload

---
+++ SumOfDigitsOddEven.java
@@ -0,0 +1,48 @@
+/*
+
+Get a number from user and find the sum of digits. Check if the sum is odd or even.
+If sum is odd print "odd", else print "even".
+
+Input Format
+Get a non-zero number from the user. The number must be an integer.
+
+Constraints
+n>0
+
+Output Format
+If sum is odd print "odd", else print "even".
+
+Sample Input 0
+543
+
+Sample Output 0
+even
+
+Sample Input 1
+124
+
+Sample Output 1
+odd
+
+ */
+
+import java.util.Scanner;
+
+public class SumOfDigitsOddEven {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int sum =0,temp;;
+        while(n!=0){
+            temp=n%10;
+            sum = sum+temp;
+            n=n/10;
+        }
+        if(sum%2==0)
+            System.out.println("even");
+        else
+            System.out.println("odd");
+    }
+    
+}
