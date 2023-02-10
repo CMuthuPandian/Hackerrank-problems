7811618b6e62f83f0a2ced9076c67d3899bc4844
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-02-10 22:09:43 +0530

    Add files via upload

---
+++ LeapYearOrNot.java
@@ -0,0 +1,42 @@
+/*
+
+Find whether the given year is leap year or not and print "Leap year"
+if it is or print "Not a leap year" if it is not.
+
+Input Format
+Integer
+
+Constraints
+year>0
+
+Output Format
+Print "Leap year" if it is a leap year else print "Not a leap year"
+
+Sample Input 0
+2020
+
+Sample Output 0
+Leap year
+
+Sample Input 1
+2003
+
+Sample Output 1
+Not a leap year
+
+ */
+
+import java.util.Scanner;
+
+public class LeapYearOrNot {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        if(n%4==0)
+            System.out.println("Leap year");
+        else
+            System.out.println("Not a leap year");
+    }
+
+}
