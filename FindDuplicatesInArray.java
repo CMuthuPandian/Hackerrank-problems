9b1413cda1a876e8de5d830de1c1701fd12f3fd5
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-02-16 20:04:08 +0530

    Update FindDuplicatesInArray.java

--- FindDuplicatesInArray.java
+++ FindDuplicatesInArray.java
@@ -29,7 +29,6 @@ Sample Output 1
  */
 import java.util.Scanner;
 public class FindDuplicatesInArray {
-
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
