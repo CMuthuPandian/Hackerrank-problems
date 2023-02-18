63355aa7659787be25219e88a57c4bd8ba840ee5
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-02-19 00:38:36 +0530

    Update CountEvenDigits.java

--- CountEvenDigits.java
+++ CountEvenDigits.java
@@ -26,12 +26,10 @@ Sample Output 1
  */
 import java.util.Scanner;
 public class CountEvenDigits {
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int a=0,b=0;
-
         while(n!=0){
             a = n%10;
             if(a%2==0)
