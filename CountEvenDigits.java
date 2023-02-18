f338cf04dbee3870c4016281126fff4ec8651fe1
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-02-19 00:37:17 +0530

    Add files via upload

---
+++ CountEvenDigits.java
@@ -0,0 +1,46 @@
+/*
+
+Count the even digits in a number. Print the count.
+
+Input Format
+Input should be an Integer
+
+Constraints
+1 <= N <= 1000000
+
+Output Format
+Output should be an Integer
+
+Sample Input 0
+123
+
+Sample Output 0
+1
+
+Sample Input 1
+2138
+
+Sample Output 1
+2
+
+ */
+
+import java.util.Scanner;
+
+public class CountEvenDigits {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a=0,b=0;
+
+        while(n!=0){
+            a = n%10;
+            if(a%2==0)
+                b++;
+            n=n/10;
+        }
+        System.out.println(b);
+    }
+
+}
