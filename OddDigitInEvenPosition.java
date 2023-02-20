a9015a3d6947ad5d4b87d9c6c3d2368ac485fa92
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-02-20 21:49:47 +0530

    Update OddDigitInEvenPosition.java

--- OddDigitInEvenPosition.java
+++ OddDigitInEvenPosition.java
@@ -30,6 +30,7 @@ Explanation 1
 The input consists only one digit. So, the ouput is 0.
 
  */
+
 import java.util.Scanner;
 public class OddDigitInEvenPosition {
     public static void main(String[] args) {
