8e17a2ad58c058a650f7dc153bf9e7897ec57621
Muthu Pandian C <93367782+CMuthuPandian@users.noreply.github.com>
2023-02-22 22:12:08 +0530

    Add files via upload

---
+++ MinElementInArray.java
@@ -0,0 +1,50 @@
+/*
+
+Find the minimum value in an array.
+
+Input Format
+Input consists one integer and one array
+
+Constraints
+1 <= N <= 100 -1000 <= arr[i] <= 1000
+
+Output Format
+Output should be the minimum element in an array
+
+Sample Input 0
+5
+1 3 2 7 6
+
+Sample Output 0
+1
+
+Sample Input 1
+7
+7 3 0 2 5 6 2
+
+Sample Output 1
+0
+
+ */
+
+import java.util.Scanner;
+
+public class MinElementInArray {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int min= Integer.MAX_VALUE;
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++)
+            arr[i]=in.nextInt();
+        for(int i =0;i<arr.length;i++){
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        System.out.println(min);
+    }
+
+}
